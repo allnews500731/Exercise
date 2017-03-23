43bfe4afa96111817e31981f4133ace1b509afd6
allnews500731 <allnews500731@gmail.com>
2017-03-23 15:42:37 +0800

    第1講

---
+++ Example01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 18.7f;
+            pokemon.Abilities = "Blaze";
+            pokemon.Category = "Lizard";
+            pokemon.Gender = "QQ";
+            Console.WriteLine("這個Pokemon的身高:{0}公分,體重:{1}公斤", pokemon.Height, pokemon.Weight);
+        }
+    }
+}
---
+++ example02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(MyBaseC.MyStruct.x);
+        }
+
+        public class MyBaseC
+        {
+            public struct MyStruct
+            {
+                public static int x = 100;
+            }
+        }
+    }
+}
